c6bef1ca6b394a025bda2cdf193d9debf63f84d9
Guilherme <guilherme_riba@hotmail.com>
2024-04-11 10:24:45 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gmf/pdvwebservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gmf.pdvwebservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gmf.pdvwebservice.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
