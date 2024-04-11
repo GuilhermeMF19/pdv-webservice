a8d17b10a8b9e36efb5ff873776c0440f131f237
Guilherme <guilherme_riba@hotmail.com>
2024-04-11 09:53:05 -0300

    Create User entity and resource

---
+++ src/main/java/com/gmf/pdvwebservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.gmf.pdvwebservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gmf.pdvwebservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L,"maria", "maria@gmail.com", "9999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
