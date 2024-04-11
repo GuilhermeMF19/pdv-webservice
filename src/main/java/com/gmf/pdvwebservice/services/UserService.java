90558c725a0e03c4e0205b7e3a695f7e3d750556
Guilherme <guilherme_riba@hotmail.com>
2024-04-11 11:10:18 -0300

    Service layer, component registration

---
+++ src/main/java/com/gmf/pdvwebservice/services/UserService.java
@@ -0,0 +1,27 @@
+package com.gmf.pdvwebservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gmf.pdvwebservice.entities.User;
+import com.gmf.pdvwebservice.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
