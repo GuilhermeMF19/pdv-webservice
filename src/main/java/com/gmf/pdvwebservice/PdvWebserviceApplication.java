a448c41c0e05b812ea17692046164392adbfe309
Guilherme <guilherme_riba@hotmail.com>
2024-04-11 09:36:55 -0300

    Project created

---
+++ src/main/java/com/gmf/pdvwebservice/PdvWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gmf.pdvwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmf/pdvwebservice/PdvWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmf.pdvwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
