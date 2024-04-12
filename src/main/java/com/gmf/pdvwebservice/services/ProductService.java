63ec4bc0c958f3139f3d1c022a0ab167e52a06c2
Guilherme <guilherme_riba@hotmail.com>
2024-04-12 15:10:20 -0300

    implements insert, delete, update and exceptions in Product

--- src/main/java/com/gmf/pdvwebservice/services/ProductService.java
+++ src/main/java/com/gmf/pdvwebservice/services/ProductService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gmf.pdvwebservice.entities.Product;
 import com.gmf.pdvwebservice.repositories.ProductRepository;
+import com.gmf.pdvwebservice.services.exceptions.DatabaseException;
+import com.gmf.pdvwebservice.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -22,6 +28,40 @@ public class ProductService {
 	
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Product update(Long id, Product obj) {
+		try {
+			Product entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
+		
+	    entity.getCategories().clear();
+	    entity.getCategories().addAll(obj.getCategories());
 	}
 }
