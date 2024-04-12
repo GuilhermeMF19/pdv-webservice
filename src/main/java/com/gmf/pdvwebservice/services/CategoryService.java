ee94a6807ae88f45bf2ca07deff4714082c8f072
Guilherme <guilherme_riba@hotmail.com>
2024-04-12 14:37:10 -0300

    implements insert, delete, update and exceptions in Category

--- src/main/java/com/gmf/pdvwebservice/services/CategoryService.java
+++ src/main/java/com/gmf/pdvwebservice/services/CategoryService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gmf.pdvwebservice.entities.Category;
 import com.gmf.pdvwebservice.repositories.CategoryRepository;
+import com.gmf.pdvwebservice.services.exceptions.DatabaseException;
+import com.gmf.pdvwebservice.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -23,5 +29,33 @@ public class CategoryService {
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
